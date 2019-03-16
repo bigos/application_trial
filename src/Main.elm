97cf472c53df87ff9c7b02e046bb50df0cfb1d9c
Jacek Podkanski <ruby.object@googlemail.com>
2019-03-16 23:58:02 +0000

    stuck

--- src/Main.elm
+++ src/Main.elm
@@ -1,31 +1,23 @@
-module Main exposing (Model, Msg(..), main, subscriptions, update, view)
+module Main exposing (Flags(..), Model, Msg(..), Note, init, main, subscriptions, update, view)
 
 import Browser exposing (..)
+import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
 import Json.Decode as D exposing (..)
-module Main exposing (Model, Msg(..), main, subscriptions, update, view)
-
-import Browser exposing (..)
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
-import Http
-import Json.Decode as D exposing (..)
-
-
-
--- MAIN
+import Url
 
 
 main =
-    Browser.document
+    Browser.application
         { init = init
-        , view = view
-        , update = update
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
         , subscriptions = subscriptions
+        , update = update
+        , view = view
         }
 
 
@@ -39,9 +31,8 @@ type alias Model =
     }
 
 
-type alias Flags =
-    --empty tuple
-    ()
+type Flags
+    = Int
 
 
 type alias Note =
@@ -53,8 +44,7 @@ type alias Note =
     }
 
 
-init : Flags -> ( Model, Cmd Msg )
-init flags =
+init _ url navkey =
     ( { message = "Hi"
       , code = 0
       }
@@ -68,12 +58,8 @@ init flags =
 
 type Msg
     = Reset
-    | MorePlease
-    | GotIt (Result Http.Error Note)
-
-
-
--- update : Msg -> Model -> ( Model, Cmd Msg )
+    | UrlChanged Url.Url
+    | LinkClicked UrlRequest
 
 
 update msg model =
@@ -81,42 +67,16 @@ update msg model =
         Reset ->
             ( model, Cmd.none )
 
-        MorePlease ->
-            ( { model | code = 1, message = "test" }, getNote )
+        UrlChanged _ ->
+            ( model, Cmd.none )
 
-        GotIt res ->
-            case res of
-                Ok gottext ->
-                    ( { model | message = Debug.toString gottext, code = 1200 }
-                    , Cmd.none
-                    )
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Internal url ->
+                    ( model, Cmd.none )
 
-                Err a ->
-                    ( { model | message = "error >>> " ++ Debug.toString a }, Cmd.none )
-
-
-getNote =
-    let
-        myurl =
-            "http://localhost:3000/notes/3"
-    in
-    Http.get
-        { expect = Http.expectJson GotIt itDecoder
-        , url = myurl
-        }
-
-
-
--- DECODER
-
-
-itDecoder =
-    D.map5 Note
-        (field "id" int)
-        (field "author" string)
-        (field "content" string)
-        (field "created_at" string)
-        (field "updated_at" string)
+                External href ->
+                    ( model, Cmd.none )
 
 
 
@@ -132,139 +92,6 @@ subscriptions model =
 -- VIEW
 
 
+view : model -> Document msg
 view model =
-    { title = "My first title"
-    , body =
-        [ div [] [ text "Hurrah!!!" ]
-        , div [] [ text <| String.fromInt model.code ]
-        , div [] [ text (Debug.toString model.message) ]
-        , div [] [ button [ onClick MorePlease ] [ text "Try Again!" ] ]
-        ]
-    }
-
-
-
--- MAIN
-
-
-main =
-    Browser.document
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
-
-
-
--- MODEL
-
-
-type alias Model =
-    { message : String
-    , code : Int
-    }
-
-
-type alias Flags =
-    --empty tuple
-    ()
-
-
-type alias Note =
-    { id : Int
-    , author : String
-    , content : String
-    , created_at : String
-    , updated_at : String
-    }
-
-
-init : Flags -> ( Model, Cmd Msg )
-init flags =
-    ( { message = "Hi"
-      , code = 0
-      }
-    , Cmd.none
-    )
-
-
-
--- UPDATE
-
-
-type Msg
-    = Reset
-    | MorePlease
-    | GotIt (Result Http.Error Note)
-
-
-
--- update : Msg -> Model -> ( Model, Cmd Msg )
-
-
-update msg model =
-    case msg of
-        Reset ->
-            ( model, Cmd.none )
-
-        MorePlease ->
-            ( { model | code = 1, message = "test" }, getNote )
-
-        GotIt res ->
-            case res of
-                Ok gottext ->
-                    ( { model | message = Debug.toString gottext, code = 1200 }
-                    , Cmd.none
-                    )
-
-                Err a ->
-                    ( { model | message = "error >>> " ++ Debug.toString a }, Cmd.none )
-
-
-getNote =
-    let
-        myurl =
-            "http://localhost:3000/notes/3"
-    in
-    Http.get
-        { expect = Http.expectJson GotIt itDecoder
-        , url = myurl
-        }
-
-
-
--- DECODER
-
-
-itDecoder =
-    D.map5 Note
-        (field "id" int)
-        (field "author" string)
-        (field "content" string)
-        (field "created_at" string)
-        (field "updated_at" string)
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
-
-
-
--- VIEW
-
-
-view model =
-    { title = "My first title"
-    , body =
-        [ div [] [ text "Hurrah!!!" ]
-        , div [] [ text <| String.fromInt model.code ]
-        , div [] [ text (Debug.toString model.message) ]
-        , div [] [ button [ onClick MorePlease ] [ text "Try Again!" ] ]
-        ]
-    }
+    { title = "New title", body = [] }
